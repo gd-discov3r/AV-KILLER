c528a67f63af65e5b86194d2e2e4c9de8c2eb902
3t3rn4l-Ch4mp10n <132711888+K3rnel-Dev@users.noreply.github.com>
2024-04-09 00:38:24 +0300

    -

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# AV-KILLER
- 🦈 Killing 1100+ antiviruses process
+# 🦈 AV-KILLER
+![CSHARP](https://img.shields.io/badge/Language-CSHARP-aqua?style=for-the-badge&logo=CS)
+![](BANNER.png)
+
+## 📑 About
+<b> Simple-tool for killing [ 1100+ ] antiviruses process</b>
+
+### 🚦 Features:
+ * Build Size: 41kb✅
+ * User-Friendly: ✅
+
+## ⚠️ Disclaimer
+ * This tool for education and purposes only!
+ * Do not use for illegal purposes!
---
+++ src/AvKiller/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("AvKiller by k3rnel-dev")]
+[assembly: AssemblyDescription("AvKiller by k3rnel-dev")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AvKiller by k3rnel-dev")]
+[assembly: AssemblyProduct("AvKiller by k3rnel-dev")]
+[assembly: AssemblyCopyright("AvKiller by k3rnel-dev")]
+[assembly: AssemblyTrademark("AvKiller by k3rnel-dev")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("41e77e65-1a85-424e-b417-2a6c1fe8e8fc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.3.7")]
+[assembly: AssemblyFileVersion("3.3.0.1")]
